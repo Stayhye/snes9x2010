8e930724b8b9671512b932ff63622030ecf62589
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-09-06 17:00:54 -0500

    Update libretro.c

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -2472,8 +2472,8 @@ static inline void s9x_ps2_fix_colors(uint16_t *pixels, int width, int height, i
         for (int x = 0; x < width; x++)
         {
             uint16_t p = row[x];
-            // Pure Red and Blue channel swap, preserving 6-bit green
-            row[x] = ((p & 0x001F) << 11) | (p & 0x07E0) | ((p & 0xF800) >> 11);
+            // 1555 layout swap: Red (10-14), Green (5-9), Blue (0-4)
+            row[x] = (p & 0x8000) | ((p & 0x001F) << 10) | (p & 0x03E0) | ((p & 0x7C00) >> 10);
         }
     }
 }
